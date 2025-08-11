69fedaa5fe07eb07183ac4c54ac930a9e640bdd0
BrunoBarlari <bruno.barlari@wbd.com>
2025-08-11 20:31:19 -0300

    Move index section to about window

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
-const isGH = process.env.DEPLOY_ENV === 'gh';
-const site = process.env.SITE_URL || (isGH ? 'https://<your-user>.github.io/<repo-name>/' : 'http://localhost');
-const base = process.env.BASE_PATH || (isGH ? '/<repo-name>/' : '/');
+const isGH = process.env.DEPLOY_ENV === "gh";
+const site =
+	process.env.SITE_URL ||
+	(isGH ? "https://<your-user>.github.io/<repo-name>/" : "http://localhost");
+const base = process.env.BASE_PATH || (isGH ? "/<repo-name>/" : "/");
 
 export default defineConfig({
-  site,
-  base,
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    sitemap()
-  ],
-  build: { format: 'directory' }
+	site,
+	base,
+	integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+	build: { format: "directory" },
 });
